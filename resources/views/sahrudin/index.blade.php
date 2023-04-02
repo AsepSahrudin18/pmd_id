9d463c43507045276431d67aa46372eaac1a0cba
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2023-04-02 18:57:12 +0700

    update page 4 - page news

--- resources/views/sahrudin/index.blade.php
+++ resources/views/sahrudin/index.blade.php
@@ -1,4 +1,73 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Ini adalah page - Asep Sahrudin</h1>
+    <div class="container mb-3">
+        <div class="col mb-4 text-center">
+            <h4>NEWS</h4>
+            <h2 class="fw-bold">Ketahui Update Terkini Tentang Kami</h2>
+        </div>
+    </div>
+    <div class="container">
+
+        {{-- inner container --}}
+        <div class="inner-container">
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+            {{-- card --}}
+            <div class="card card-size">
+                <div class="card-body">
+                    <img class="events" src="{{ url('img/news-events/event-01.png') }}" alt="event-image">
+                </div>
+            </div>
+            {{-- end card --}}
+
+        </div>
+        {{-- end inner container --}}
+    </div>
 @endsection
